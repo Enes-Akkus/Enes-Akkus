d728eb42ab31a6c51228ed499a3c7853f67956c0
Enes-Akkus <muhammedenes112233@gmail.com>
2026-06-18 11:05:48 +0300

    Add files via upload

---
+++ Program12.cs
@@ -0,0 +1,48 @@
+﻿namespace PROJE20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int DelayTime = 100;
+                for (int i = 1; i <= 100; i++)
+                {
+                    Console.Clear();
+                    Console.WriteLine("                    -");
+                    Thread.Sleep(DelayTime);
+                    Console.Clear();
+                    Console.WriteLine("                    |");
+                    Thread.Sleep(DelayTime);
+                    Console.Clear();
+                    Console.WriteLine("                    /");
+                    Thread.Sleep(DelayTime);
+                    Console.Clear();
+                    Console.WriteLine("                    -");
+                    Thread.Sleep(DelayTime);
+                    Console.Clear();
+                    Console.WriteLine("                    |");
+                    Thread.Sleep(DelayTime);
+                    Console.Clear();
+                    Console.WriteLine("                    -");
+                    Thread.Sleep(DelayTime);
+                    Console.Clear();
+                    Console.WriteLine("                    \\");
+                    Thread.Sleep(DelayTime);
+                    Console.Clear();
+                    Console.WriteLine("                    |");
+                    Thread.Sleep(DelayTime);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("HATA : {0}", ex.ToString());
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
